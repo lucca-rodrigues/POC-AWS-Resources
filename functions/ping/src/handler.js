c39bf0c98e3890716f0a7b7b2f632159edd51516
Lucas Rodrigues <lucas.souza@futuroprevidencia.com.br>
2026-09-16 17:32:16 -0300

    chore: remove function ping (nao era cenario util)

--- functions/ping/src/handler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const STAGE = process.env.STAGE ?? 'dev';
-
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ message: 'pong', stage: STAGE }),
-});
